6a04603822769eba2f69a8c6f781b7d6469398d6
manwithoutsleep <brad@southcountysound.com>
2025-08-31 16:30:33 -0500

    feat: Remove Angular Material from test code

--- src/app/components/shared/shared.fake.module.ts
+++ src/app/components/shared/shared.fake.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { AppMaterialModule } from './app.material.module';
 import { CommonModule } from '@angular/common';
 import { ErrorPageFakeComponent } from './error-page/error-page.fake.component';
 
@@ -11,11 +10,9 @@ import { ErrorPageFakeComponent } from './error-page/error-page.fake.component';
         ErrorPageFakeComponent
     ],
     imports: [
-        AppMaterialModule,
         CommonModule
     ],
     exports: [
-        AppMaterialModule,
         CommonModule,
         ErrorPageFakeComponent
     ]
